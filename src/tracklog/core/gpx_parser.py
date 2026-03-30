e0f7a2b138d1774d9426dc69585b3f7c8c485570
mantis <morauszki.andras@gmail.com>
2026-03-30 22:10:39 +0200

    core: Function to parse directory with .gpx files and handle sports without elevation

--- src/tracklog/core/gpx_parser.py
+++ src/tracklog/core/gpx_parser.py
@@ -1,9 +1,13 @@
 import math
 import gpxpy
+from pathlib import Path
 from tracklog.db.models import Workout
 
 
-def parse_gpx(file_path: str) -> Workout:
+SPORTS_WITH_ELEVATION = ["running", "hiking", "walking", "cycling"]
+
+
+def parse_gpx(file_path: Path) -> Workout:
     with open(file_path, "r") as file:
         gpx = gpxpy.parse(file)
 
@@ -15,8 +19,14 @@ def parse_gpx(file_path: str) -> Workout:
     workout_data["start_lon"] = start_coordinates.longitude
     moving_data = gpx.get_moving_data()
     distance = moving_data.moving_distance
-    elevation = gpx.get_uphill_downhill().uphill
     workout_data["distance_km"] = round(moving_data.moving_distance) / 1000
+
+    elevation = (
+        gpx.get_uphill_downhill().uphill
+        if workout_data["type"] in SPORTS_WITH_ELEVATION
+        else 0
+    )
+
     workout_data["elevation_m"] = math.floor(elevation)
     workout_data["grade"] = round(100 * elevation / distance, 2)
     workout_data["moving_time_sec"] = round(moving_data.moving_time, 3)
@@ -24,8 +34,20 @@ def parse_gpx(file_path: str) -> Workout:
         moving_data.moving_time / 60
     ) / workout_data["distance_km"]
 
-    for key, value in workout_data.items():
-        print(f"{key}: {value} ({type(value)})")
-
     workout = Workout(**workout_data)
     return workout
+
+
+def process_dir(dir_path: Path) -> list[Workout]:
+    parsed_workouts = []
+    print(f"Processing directory: {dir_path}")
+    for subpath in dir_path.iterdir():
+        if subpath.is_dir():
+            parsed_workouts.extend(process_dir(subpath))
+        elif subpath.is_file() and subpath.suffix == ".gpx":
+            print(f"Parsing: {subpath}")
+            parsed_workouts.append(parse_gpx(subpath))
+        else:
+            print(f"Skipping file: {subpath}")
+            continue
+    return parsed_workouts
